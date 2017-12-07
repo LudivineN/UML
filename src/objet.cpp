d1a9f767fea35de85785f7ea56e1cf23419ed394
MalvinaGontard <malvina.gontard@alumni.univ-avignon.fr>
2017-12-07 12:16:24 +0100

    Update objet.cpp

--- src/objet.cpp
+++ src/objet.cpp
@@ -4,14 +4,36 @@
 
 using namespace std;
 
-Objet :: Objet(string name, int case_x, int case_y, int p, int c, bool visib)
+Objet :: Objet(string name, int case_x, int case_y)
 {
 	nameobj = name;
 	x = case_x;
 	y = case_y;
-	portee = p;
-	chance = c;
-	visibilite = visib;
+	if (nameobj == "pelle")
+	{
+		portee = 0;
+		chance = 0;
+		visibilite = true;
+	}
+	if (nameobj == "mousquet")
+	{
+		portee = 1;
+		chance = 50;
+		visibilite = true;
+	}
+	if (nameobj == "tresor")
+	{
+		portee = -1;
+		chance = 0;
+		visibilite = false;
+	}	
+	if (nameobj == "armure")
+	{
+		portee = -1;
+		chance = 90;
+		visibilite = true;
+	}
+
 }
 
 string Objet :: getNameobj()
