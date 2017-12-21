842d9d0a939eb7f39ae4245b9665677f37e72e8e
MalvinaGontard <malvina.gontard@alumni.univ-avignon.fr>
2017-12-21 23:42:55 +0100

    Add files via upload

--- inc/objet.h
+++ inc/objet.h
@@ -13,14 +13,36 @@ class Objet{
 		int chance;
 		bool visibilite;
 	public:
+		
+		//! Constructeur qu'avec certains argument
+		/*!
+			\param name Le nom de l'objet
+			\param case_x La ligne où se trouve l'objet
+			\param case_y La colonne où se trouve l'objet
+		*/
 		Objet(string name, int case_x, int case_y);
+		//! Getter pour le nom.
 		string getNameobj();
+		//! Getter pour la ligne.
 		int getCase_x();
+		//! Getter pour la colonne.
 		int getCase_y();
+		//! Getter pour la portée.
 		int getPortee();
+		//! Getter pour le pourcentage de chance.
 		int getChance();
+		//! Getter pour pour la visibilité
 		bool getVisibilite();	
-Objet (string  nameobj, int x, int y, int portee, int chance, bool visibilite) ;
+		//! Constructeur avec tous les arguments
+		/*!
+			\param name Le nom de l'objet
+			\param case_x La ligne où se trouve l'objet
+			\param case_y La colonne où se trouve l'objet
+			\param portee La portée qu'a un objet
+			\param chance La chance de rester en vie et de tuer l'adversaire
+			\param visibilite La visibilité des objets
+		*/
+		Objet (string  nameobj, int x, int y, int portee, int chance, bool visibilite) ;
 
 };
 
